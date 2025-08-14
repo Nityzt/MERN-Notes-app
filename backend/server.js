03e4a1a6660b8aec704711570e8d07e48b5daf4d
Nityum <nitvfx@gmail.com>
2025-08-13 20:28:00 -0400

    deployment prep

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const app = express();
 const {connectDB} = require('./config/db.js');
 const dotenv = require('dotenv');
+const path = require("path");
 const notesRoutes = require('./routes/notesRoutes.js'); // Assuming you have a notes route file
 const ratelimiter = require('./middleware/rateLimiter.js');
 const cors = require('cors');
 dotenv.config({
     quiet: true
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 
-app.use(cors(process.env.CORS_ORIGIN || 'http://localhost:5173'));
+if(process.env.NODE_ENV !== "production"){
+app.use(cors(process.env.CORS_ORIGIN || 'http://localhost:5173'));}
 app.use(express.json()); 
 app.use(express.urlencoded({extended:false}));
 app.use(ratelimiter); // Apply rate limiting middleware
@@ -20,6 +22,14 @@ app.use((req,res,next)=>{
 })
 
 app.use('/api/notes', notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
 
 connectDB().then(()=>{
 app.listen(PORT, ()=>
